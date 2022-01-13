e0905cf1d217c68b33943c4ca66a896b39628384
Anjan Dhungana <anjan2052@gmail.com>
2022-01-13 23:58:48 +0545

    code refacoring, changes js, adds onclick animations

--- scripts.js
+++ scripts.js
@@ -1,35 +1,23 @@
-const clickSignup = () => {
-  document.getElementById("side").style =
-    "animation-name:slideLeft, stretch; animation-duration:1.5s; animation-timing-function:ease; animation-fill-mode:forwards";
-  let res = document.querySelectorAll("#side-signup-head, #side-signup-p");
-  for (let i = 0; i < res.length; i++) {
-    res[i].style =
-      "animation-name:fade-out; animation-duration:1s; animation-timing-function:ease; animation-fill-mode:forwards;";
-  }
-  let ressec = document.querySelectorAll("#side-signin-head, #side-signin-p");
-  document.getElementById('')
-  for (let i = 0; i < res.length; i++) {
-    ressec[i].style =
-      "animation-name:fade-in; animation-duration:1s; animation-timing-function:ease; animation-fill-mode:forwards;";
-  }
-  document.getElementById("main").style =
-    "animation-name:slideRight, fade-out; animation-duration:1.5s; animation-timing-function:ease; animation-fill-mode:forwards;";
-  document.getElementById("signUp-side-btn").style =
-    "animation:fade-out 2s ease; animation-fill-mode:forwards;";
-  document.getElementById("signIn-side-btn").classList.remove("d-none");
-  document.getElementById('side-signup-p').classList.add('d-none');
-  document.getElementById('side-signin-p').classList.remove('d-none');
-  document.getElementById('side-signup-head').classList.add('d-none');
-  document.getElementById('side-signin-head').classList.remove('d-none');
-  document.getElementById('mainUp').classList.remove('d-none');
-  document.getElementById('mainUp').style = "animation-name:fade-in, slidemainRight; animation-duration:1.5s; animation-fill-mode:forwards;";
-  document.getElementById('cross').style = 'color:#333';
+const signUpBtn = document.getElementById("signUpbtn");
+
+const clickSignUp = () => {
+  document.getElementById("overlay").classList.add("toRight");
+  document.getElementById("sidebar").classList.add("toLeft");
+  document.getElementById("main").classList.add('mainRight','decZindex');
+  document.getElementById("mainUp").classList.add('mainRight');
 
 
-//   document.getElementById('')
+  
+
 };
 
-const clickSignIn =()=>{
-    // document.getElementById(signIn-side-btn).
-    
+const clickSignIn = ()=>{
+  document.getElementById('overlay').classList.remove('toRight');
+  document.getElementById("sidebar").classList.remove("toLeft");
+  document.getElementById("main").classList.remove('mainRight','decZindex')
+  document.getElementById("mainUp").classList.remove('mainRight');
+  
+  
 }
+
+
